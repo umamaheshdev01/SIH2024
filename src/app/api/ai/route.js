47092a15620f351347e062362f80c05f7526fdc9
Your Name <you@example.com>
2024-08-30 00:12:20 +0530

    'ok'

--- src/app/api/ai/route.js
+++ src/app/api/ai/route.js
@@ -1,55 +1,18 @@
-import { createClient } from '@supabase/supabase-js'
-const supabaseUrl = 'https://hwnkucfptcbnpbmbmtec.supabase.co'
-const supabaseKey ='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh3bmt1Y2ZwdGNibnBibWJtdGVjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDkzODExNzEsImV4cCI6MjAyNDk1NzE3MX0.QTCWf1l7vkryMi6DPWDckOLU5ft8tzclpSkZr3zzxQU'
-const supabase = createClient(supabaseUrl, supabaseKey)
-import { NextResponse } from "next/server"
+import { NextResponse } from "next/server";
+import { GoogleGenerativeAI } from "@google/generative-ai";
 
+export const POST = async (req, res) => {
+  try {
+    const { prompt } = await req.json();
 
+    const genAI = new GoogleGenerativeAI(process.env.API_KEY);
+    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+    const result = await model.generateContent('Answer question from paragraph below only dont give extra if you dont know say i dont know '+prompt);
 
-export const POST = async(req,res)=>{
-    
-    // create Class
-    const data1 = await req.json()
+    const data = result.response.text()
 
-    const {error} = await supabase.from('Class').insert(data1)
-
-    const {data} = await supabase.from('Class').select('').eq('name',data1.name).eq('admin',data1.admin)
-
-
-    const pack = {
-        class : data[0].code,
-        user : data1.admin,
-        role : 0
-    }
-
-    const {dat1,err1} = await supabase.from('Member').insert(pack)
-
-    if(error)
-    {
-        return NextResponse.json({error},{status:500})
-    }
-    else
-    {
-        return NextResponse.json({msg:'Class created and Admin set'},{status:200})
-    }
-
-}
-
-
-export const GET = async(req,res)=>{
-    
-    //allClassesofaperson
-    const {id} = Object.fromEntries(new URL(req.url).searchParams.entries());
-    console.log(id)
-
-    const {data,error} = await supabase.from('Member').select('*,name:Class(*)').eq('user',id)
-
-    if(error)
-    {
-        return NextResponse.json({error},{status:500})
-    }
-    else
-    {
-        return NextResponse.json({data},{status:200})
-    }
-}
+    return NextResponse.json({ data }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+};
